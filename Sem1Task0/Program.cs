7460129476f032ae1649e2bd091b9f9dfdce5049
Golikov-Andrey <newsc2@yandex.ru>
2022-11-29 22:02:05 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if(inputLine!=null)
+{
+   //Парсим введенное число
+   int inputNumber = int.Parse(inputLine);
+
+   //Находим квадрат числа
+   int outNumber = inputNumber*inputNumber;
+   //int outNumber = (int)Math.Pow(inputNumber,2);
+
+
+   // Выводим данные в консоль 
+   Console.WriteLine(outNumber);
+   //Console.WriteLine(Math.Pow(int.Parse(inputLine),2));
+}
