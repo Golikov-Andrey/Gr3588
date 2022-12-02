0b180d8bbf64fd36eb894aad3fd692b0cf5ce685
Golikov-Andrey <newsc2@yandex.ru>
2022-12-02 22:12:12 +0300

    add solution for task 8,9,11,12,14,16

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -1,19 +1,37 @@
 ﻿
+// //Считываем данные с консоли
+// string? inputLine = Console.ReadLine();
+
+// //Проверяем, чтобы данные были не пустыми
+// if(inputLine!=null)
+// {
+//    //Парсим введенное число
+//    int inputNumber = int.Parse(inputLine);
+
+//    //Находим квадрат числа
+//    int outNumber = inputNumber*inputNumber;
+//    //int outNumber = (int)Math.Pow(inputNumber,2);
+
+
+//    // Выводим данные в консоль 
+//    Console.WriteLine(outNumber);
+//    //Console.WriteLine(Math.Pow(int.Parse(inputLine),2));
+// }
+
 //Считываем данные с консоли
-string? inputLine = Console.ReadLine();
+string? num1Line = Console.ReadLine();
 
-//Проверяем, чтобы данные были не пустыми
-if(inputLine!=null)
+ //Парсим введенные числа
+int startNumber = int.Parse(num1Line);
+
+int num2 = 2;
+
+ //Выходное значение
+string outLine = string.Empty;
+
+while (num2 <= startNumber)
 {
-   //Парсим введенное число
-   int inputNumber = int.Parse(inputLine);
-
-   //Находим квадрат числа
-   int outNumber = inputNumber*inputNumber;
-   //int outNumber = (int)Math.Pow(inputNumber,2);
-
-
-   // Выводим данные в консоль 
-   Console.WriteLine(outNumber);
-   //Console.WriteLine(Math.Pow(int.Parse(inputLine),2));
+    outLine = outLine + num2 + ", ";
+    num2 = num2 + 2;
 }
+Console.WriteLine(outLine);
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿string? inputLine = Console.ReadLine();             // Считываем данные с консоли 
+
+
+if(inputLine!=null)                                 // проверяем, чтобы данные были не пустыми     
+{
+    int inputNumber = int.Parse( inputLine);        // Изменяем введенную строку в целое число
+    int index = 2;                                  // счетчик для цикла, стартуем с 2, так как нужны только четные числа
+    
+    string outLine = string.Empty;
+
+    while ( index <= inputNumber-2)                  // цикл до (введенного числа+1), чтоб если введенное число четное, то оно попадет в строку  
+    {
+       outLine = outLine + index + ',';             // приписываем четные числа
+       index = index+2; 
+    }
+    outLine=outLine+index;
+    //outLine = outLine.TrimEnd(',');
+
+    Console.WriteLine(outLine);
+}
